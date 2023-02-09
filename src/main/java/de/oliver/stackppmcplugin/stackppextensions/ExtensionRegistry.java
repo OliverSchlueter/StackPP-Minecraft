ddea9b69509dc3d29e8c5c56b753a4f6787a3040
OliverSchlueter <oliverhd.1303@gmail.com>
2023-02-09 19:46:07 +0100

    Remove syscalls that can not be used

--- src/main/java/de/oliver/stackppmcplugin/stackppextensions/ExtensionRegistry.java
+++ src/main/java/de/oliver/stackppmcplugin/stackppextensions/ExtensionRegistry.java
@@ -8,8 +8,18 @@ public class ExtensionRegistry {
     private static SendMessageToPlayerSyscall sendMessageToPlayerSyscall;
 
     public static void registerSyscalls(Machine machine){
+        // add custom syscalls
         sendMessageToPlayerSyscall = new SendMessageToPlayerSyscall(100, machine);
         machine.getSyscalls().put(100, sendMessageToPlayerSyscall);
+
+        // remove graphics syscalls
+        machine.getSyscalls().remove(6);
+        machine.getSyscalls().remove(7);
+        machine.getSyscalls().remove(8);
+        machine.getSyscalls().remove(9);
+
+        // remove read line syscall
+        machine.getSyscalls().remove(11);
     }
 
     public static SendMessageToPlayerSyscall getSendMessageToPlayerSyscall() {
